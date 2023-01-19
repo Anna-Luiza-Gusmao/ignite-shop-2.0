b340665edc215de5bb77bb85049c866b229a6d10
anna-luiza-haus <luizasgusmao@hotmail.com>
2023-01-19 13:15:00 -0300

    AddCarousel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,42 @@
 import { HomeContainer, Product } from "@/styles/pages/home"
 import Image from "next/legacy/image"
+import { useKeenSlider } from 'keen-slider/react'
+import 'keen-slider/keen-slider.min.css'
 
 import Shirt from '../assets/shirt.png'
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48
+    }
+  })
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image src={Shirt} width={480} height={520} alt="" />
         <footer>
           <strong>Camiseta X</strong>
           <span>R$ 79,90</span>
         </footer>
       </Product>
-      <Product>
+      <Product className="keen-slider__slide">
+        <Image src={Shirt} width={480} height={520} alt="" />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={Shirt} width={480} height={520} alt="" />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
         <Image src={Shirt} width={480} height={520} alt="" />
         <footer>
           <strong>Camiseta X</strong>
