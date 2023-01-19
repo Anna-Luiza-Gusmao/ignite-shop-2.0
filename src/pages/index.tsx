93f97b0202c923b81650e23175deed4729a72116
anna-luiza-haus <luizasgusmao@hotmail.com>
2023-01-19 10:22:33 -0300

    AddHeader

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,7 @@
-import { styled } from '../styles'
-
-const Button = styled('button', {
-  backgroundColor: '#c24040',
-
-  span: {
-    fontWeight: 'bold'
-  },
-
-  '&:hover': {
-    backgroundColor: '#c76f6f',
-  }
-})
-
 export default function Home() {
   return (
-    <Button>
-      <span>oi</span>
-      Hello
-    </Button>
+    <div>
+
+    </div>
   )
 }
