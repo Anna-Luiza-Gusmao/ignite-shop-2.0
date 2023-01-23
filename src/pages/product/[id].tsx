30978f1e4677690bbb5b0080e4126f6c480b22f5
anna-luiza-haus <luizasgusmao@hotmail.com>
2023-01-23 09:08:18 -0300

    LoadingEachProductOnThePage

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,85 @@
-import { useRouter } from 'next/router'
+import { stripe } from "@/lib/stripe"
+import { ImageContainer, ProductContainer, ProductDetails } from "@/styles/pages/product"
+import { GetStaticPaths, GetStaticProps } from "next"
+import Image from "next/legacy/image"
+import { useRouter } from "next/router"
+import Stripe from "stripe"
 
-export default function Product () {
-    const { query } = useRouter()
+interface ProductProps {
+    product: {
+        id: string,
+        name: string,
+        imageUrl: string,
+        price: string,
+        description: string
+    }
+}
+
+export default function Product({ product }: ProductProps) {
+    const { isFallback } = useRouter()
+
+    if(isFallback) return <p>Loading...</p>
 
     return (
-        <h1>Product </h1>
+        <ProductContainer>
+            <ImageContainer>
+                <Image src={product.imageUrl} width={520} height={480} alt="" />
+            </ImageContainer>
+
+            <ProductDetails>
+                <h1>{product.name}</h1>
+                <span>{product.price}</span>
+
+                <p>{product.description}</p>
+
+                <button>Comprar agora</button>
+            </ProductDetails>
+        </ProductContainer>
     )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+    return {
+        paths: [
+            { params: {
+                id: 'prod_NCPwKLy3R8yHQe'
+            } }
+        ],
+        fallback: true
+    }
+}
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ params }) => {
+    if (params != undefined) {
+        const productId = params.id
+        const product = await stripe.products.retrieve(productId, {
+            expand: ['default_price']
+        })
+
+        const price = product.default_price as Stripe.Price
+        let formattedPrice = ''
+
+        if (price.unit_amount != null) {
+            formattedPrice = new Intl.NumberFormat('pt-BR', {
+                style: 'currency',
+                currency: 'BRL'
+            }).format(price.unit_amount / 100)
+        }
+
+        return {
+            props: {
+                product: {
+                    id: product.id,
+                    name: product.name,
+                    imageUrl: product.images[0],
+                    price: formattedPrice,
+                    description: product.description
+                }
+            },
+            revalidate: 60 * 60 * 1
+        }
+    }
+    return {
+        props: {}
+    }
 }
